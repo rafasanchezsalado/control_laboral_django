b23fde27b03cc606880ce49601576e62ed374852
rafaelsanchezsalado <rafael.sanchez.salado@gmail.com>
2020-09-20 12:51:32 +0200

    settings

--- control_laboral/control_laboral/settings.py
+++ control_laboral/control_laboral/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from secrets import *
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
