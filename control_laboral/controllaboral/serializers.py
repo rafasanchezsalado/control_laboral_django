4aa9b1f21404a22b43aac56b042c988038d6b325
rafaelsanchezsalado <rafael.sanchez.salado@gmail.com>
2020-09-25 17:08:17 +0200

    fixing the api

--- control_laboral/controllaboral/serializers.py
+++ control_laboral/controllaboral/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
-from .models import Employee
+from .models import *
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.HyperlinkedRelatedField(
-        many=False, view_name="question-detail", read_only=True
+        many=False, view_name="employee-detail", read_only=True
     )
 
     class Meta:
         model = Employee
-        fields = ["name", "nif", "date_birth", "email"]
+        # fields = ["name", "nif", "date_birth", "email"]
+        fields = '__all__'
