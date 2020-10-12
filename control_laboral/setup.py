07fb8c5864e748e912a31f2aec852abbc16428d0
rafaelsanchezsalado <rafael.sanchez.salado@gmail.com>
2020-10-12 19:21:22 +0200

    run test with tox

---
+++ control_laboral/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='controllaboral',
+    description='Control Laboral',
+
+    license='GPL-3.0-or-later',
+
+    version='0.1',
+
+    author='Rafael Sanchez',
+
+    packages=(setuptools.find_packages()),
+)
