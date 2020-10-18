ae395e3ccc1d86de685a5288ba264b77c0e1ac3f
rafaelsanchezsalado <rafael.sanchez.salado@gmail.com>
2020-10-18 20:28:51 +0200

    add id and url to the api

--- control_laboral/controllaboral/serializers.py
+++ control_laboral/controllaboral/serializers.py
@@ -13,14 +13,9 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.HyperlinkedRelatedField(
-        many=False, view_name="employee-detail", read_only=True
-    )
-
     class Meta:
         model = Employee
-        # fields = ["name", "nif", "date_birth", "email"]
-        fields = "__all__"
+        fields = ["url", "id", "name", "nif", "date_birth", "email"]
 
 
 class HolidaySerializer(serializers.HyperlinkedModelSerializer):
